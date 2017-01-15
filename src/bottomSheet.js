b0ecec9887782a3df7972702120548d90aaf7c7d
Sascha Becker <s.becker@wertarbyte.com>
2017-01-15 20:17:59 +0100

    Add basic grey overlay with bottom container and onclose action.

---
+++ src/bottomSheet.js
@@ -0,0 +1,55 @@
+import React, { Component, PropTypes } from 'react'
+import { Paper } from 'material-ui'
+
+const styles = {
+  root: {
+    height: '100%',
+    width: '100%',
+    position: 'fixed',
+    zIndex: 1300,
+    left: 0,
+    top: 0,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    transition: 'opacity 400ms cubic-bezier(0.4, 0, 0.2, 1)'
+  },
+  content: {
+    width: '100%',
+    maxHeight: 600,
+    position: 'fixed',
+    bottom: 0
+  }
+}
+
+export class BottomSheet extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          ...styles.root,
+          pointerEvents: this.props.open ? null : 'none',
+          opacity: this.props.open ? '1' : '0',
+          ...this.props.style
+        }}
+        onTouchTap={this.props.onRequestClose}
+      >
+        <Paper style={styles.content}>
+          {this.props.children}
+        </Paper>
+      </div>
+    )
+  }
+}
+
+BottomSheet.defaultProps = {
+  onRequestClose: () => {},
+  open: false
+}
+
+BottomSheet.propTypes = {
+  onRequestClose: PropTypes.func.isRequired,
+  open: PropTypes.bool
+}
---
+++ stories/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import { AppBar, List, ListItem, Paper, Subheader } from 'material-ui'
+import { action, storiesOf } from '@kadira/storybook'
+import { BottomSheet } from '../src'
+import { themed } from './storyUtil'
+
+const styles = {
+  card: {
+    padding: 24,
+    marginBottom: 24
+  }
+}
+
+storiesOf('Mobile', module)
+  .add('Normal', () => themed(
+    <div>
+      <AppBar/>
+      <div style={{ padding: 24 }}>
+        <Paper style={styles.card}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+          clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </Paper>
+        <Paper style={styles.card}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+          clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </Paper>
+        <Paper style={styles.card}>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+          clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,
+          consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+          takimata sanctus est Lorem ipsum dolor sit amet.
+        </Paper>
+      </div>
+      <BottomSheet
+        onRequestClose={action('onClose')}
+        open
+      >
+        <Subheader>Open in</Subheader>
+        <List>
+          <ListItem primaryText="Google Plus" />
+          <ListItem primaryText="Facebook" />
+          <ListItem primaryText="Inbox" />
+          <ListItem primaryText="TextEditor" />
+        </List>
+      </BottomSheet>
+    </div>
+  ))
